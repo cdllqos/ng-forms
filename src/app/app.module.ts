4449685635269bc0155860e02676073813b8c021
ChenDe <cdllqos@outlook.com>
2018-12-27 12:34:26 +0800

    [demo]:add service worker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LtFormModule } from 'lt-forms';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, LtFormModule.forRoot()],
+  imports: [BrowserModule, AppRoutingModule, LtFormModule.forRoot(), ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })],
   providers: [],
   bootstrap: [AppComponent],
 })
