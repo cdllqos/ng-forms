afd4cf536b8d4f79735d7ff3024b33b45efe0c2c
ChenDe <cdllqos@outlook.com>
2019-01-10 00:22:00 +0800

    feat: support address field submit value

--- projects/lt-forms/src/lib/components/address/address.component.ts
+++ projects/lt-forms/src/lib/components/address/address.component.ts
@@ -9,6 +9,9 @@ import {city} from './city';
 import {province} from './province';
 
 const emptyData = [{name: '暂无数据'}];
+const PROVINCE = 'province';
+const CITY = 'city';
+const AREA = 'area';
 
 @Component({
   selector: 'lt-address',
@@ -22,6 +25,8 @@ export class AddressComponent extends BaseField implements OnInit {
   provinceModel: FieldModel;
   cityModel: FieldModel;
   areaModel: FieldModel;
+  nameResultMap: Map<string, string> = new Map<string, string>();
+  valueResultMap: Map<string, string> = new Map<string, string>();
 
   adressVal: SelectItem;
   constructor() {
@@ -33,37 +38,64 @@ export class AddressComponent extends BaseField implements OnInit {
       return {name: m.name, value: m.id};
     });
     this.buildSelectList();
+    this.initialVal();
   }
 
   buildSelectList() {
     this.provinceModel =
-        this.buildFieldModel('province', '省', this.provinceList);
-    this.cityModel = this.buildFieldModel('city', '城市', emptyData);
-    this.areaModel = this.buildFieldModel('area', '区县', emptyData);
+        this.buildFieldModel(PROVINCE, '省', this.provinceList);
+    this.cityModel = this.buildFieldModel(CITY, '城市', emptyData);
+    this.areaModel = this.buildFieldModel(AREA, '区县', emptyData);
   }
 
   onProvinceChange(provinceId) {
-    if (!provinceId) {
+    const selectedProvince =
+        this.provinceList.find(m => m.value === provinceId);
+    if (!selectedProvince) {
+      this.cityModel = this.buildFieldModel(CITY, '城市', emptyData);
+      this.areaModel = this.buildFieldModel(AREA, '区县', emptyData);
+      this.setFieldVal(PROVINCE, '', '');
       return;
     }
+    this.setFieldVal(PROVINCE, selectedProvince.name, selectedProvince.value);
     this.cityList = city[provinceId].map(m => {
       return {name: m.name, value: m.id};
     });
-    this.cityModel = this.buildFieldModel('city', '城市', this.cityList);
-    this.areaModel = this.buildFieldModel('area', '区县', emptyData);
+    this.cityModel = this.buildFieldModel(CITY, '城市', this.cityList);
+    this.areaModel = this.buildFieldModel(AREA, '区县', emptyData);
   }
 
   onCityChange(cityId) {
-    if (!cityId) {
+    const selectedCity = this.cityList.find(m => m.value === cityId);
+    if (!selectedCity) {
+      this.areaModel = this.buildFieldModel(AREA, '区县', this.areaList);
+      this.setFieldVal(CITY, '', '');
       return;
     }
+    this.setFieldVal(CITY, selectedCity.name, selectedCity.value);
     this.areaList = area[cityId].map(m => {
       return {name: m.name, value: m.id};
     });
-    this.areaModel = this.buildFieldModel('area', '区县', this.areaList);
+    this.areaModel = this.buildFieldModel(AREA, '区县', this.areaList);
   }
 
-  onAreaChange(areaId) {}
+  onAreaChange(areaId) {
+    const selectedArea = this.areaList.find(m => m.value === areaId);
+    if (!selectedArea) {
+      this.setFieldVal(AREA, '', '');
+      return;
+    }
+    this.setFieldVal(AREA, selectedArea.name, selectedArea.value);
+  }
+
+  private initialVal() {
+    this.nameResultMap.set(PROVINCE, '');
+    this.nameResultMap.set(CITY, '');
+    this.nameResultMap.set(AREA, '');
+    this.valueResultMap.set(PROVINCE, '');
+    this.valueResultMap.set(CITY, '');
+    this.valueResultMap.set(AREA, '');
+  }
 
   private buildFieldModel(key: string, label: string, list: Array<SelectItem>):
       FieldModel {
@@ -77,5 +109,41 @@ export class AddressComponent extends BaseField implements OnInit {
     };
   }
 
-  private setFieldVal() {}
+  private setFieldVal(key: string, name: string, value: string) {
+    switch (key) {
+      case PROVINCE: {
+        this.nameResultMap.set(PROVINCE, name);
+        this.nameResultMap.set(CITY, '');
+        this.nameResultMap.set(AREA, '');
+        this.valueResultMap.set(PROVINCE, value);
+        this.valueResultMap.set(CITY, '');
+        this.valueResultMap.set(AREA, '');
+        break;
+      }
+      case CITY: {
+        this.nameResultMap.set(CITY, name);
+        this.nameResultMap.set(AREA, '');
+        this.valueResultMap.set(CITY, value);
+        this.valueResultMap.set(AREA, '');
+        break;
+      }
+      case AREA: {
+        this.nameResultMap.set(AREA, name);
+        this.valueResultMap.set(AREA, value);
+        break;
+      }
+      default: { break; }
+    }
+    let resultName = '';
+    let resultValue = '';
+    this.nameResultMap.forEach((m) => {
+      resultName += `${m}-`;
+    });
+    resultName = resultName.substring(0, resultName.length - 1);
+    this.valueResultMap.forEach((m) => {
+      resultValue += `${m}-`;
+    });
+    resultValue = resultValue.substring(0, resultValue.length - 1);
+    this.ctrl.setValue({name: resultValue, value: resultValue});
+  }
 }
